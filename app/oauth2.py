abaf7cedeb7063315553a27df4e7828d8f0f4e75
Chrysler1211 <chrysler.clarence19@gmail.com>
2026-09-20 09:35:54 +0800

    set all variables to lowercase

--- app/oauth2.py
+++ app/oauth2.py
@@ -19,16 +19,16 @@ SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-
 def create_token(data: dict):
 
     to_encode = data.copy()
 
-    Expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": Expire})
 
-    token = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode({"exp": expire})
 
+    token = jwt.encode(key=SECRET_KEY, algorithm=ALGORITHM, payload=to_encode)
+    
     return token
 
 
